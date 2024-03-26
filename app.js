2373f5f4022bf8eddc36aac1b0dfd3321b680aa0
Zaid khan <160598429+Zaidkhan009@users.noreply.github.com>
2024-03-27 02:02:42 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function city_country(city, country) {
+    return "".concat(city, ", ").concat(country);
+}
+var c1 = city_country('lahore', 'pskistan');
+var c2 = city_country('tokyo', 'japan');
+var c3 = city_country('paris', 'france');
+console.log(c1);
+console.log(c2);
+console.log(c3);
---
+++ app.ts
@@ -0,0 +1,11 @@
+// function city_country(city: string, country: string): string {
+//     return `${city}, ${country}`
+// }
+
+// let c1 = city_country('lahore' , 'pskistan')
+// let c2 = city_country('tokyo' , 'japan')
+// let c3 = city_country('paris' , 'france')
+
+// console.log(c1)
+// console.log(c2)
+// console.log(c3)
